48d118781f17f2ebc29d6918ac180e948f709322
Ankit Prasad <ankit28prasad9@gmail.com>
2022-03-06 20:21:33 +0530

    Update Program.cs

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace TicTacToe
 {
@@ -9,32 +9,48 @@ namespace TicTacToe
             Grid game = new Grid();
             game.createGrid();
             game.displayGrid();
-            int choice1, choice2,next=0;
+            int choice1, choice2, next = 0;
+            int draw = 0;
             while (true)
             {
                 next = 0;
                 while (true)
                 {
-                    Console.Write("\nPlayer-1 ->Enter box number to grab it: ");
-                    bool valid = int.TryParse(Console.ReadLine(), out choice1);
-                    if (valid && choice1>0&&choice1<=9)
+                    if (draw < 9)
+                        Console.Write("\nPlayer-1 ->Enter box number to grab it: ");
+                    else
                     {
-                        int check=game.playChance(1,choice1);
+                        Console.WriteLine("ROUND DRAW!!!.\nPlease press any key to restart!!");
+                        Console.ReadKey();
+                        Console.Clear();
+                        game.createGrid();
+                        game.displayGrid();
+                        draw = 0;
+                        break;
+                    }
+                    bool valid = int.TryParse(Console.ReadLine(), out choice1);
+                    if (valid && choice1 > 0 && choice1 <= 9)
+                    {
+                        int check = game.playChance(1, choice1);
                         switch (check)
                         {
-                            case 1: Console.WriteLine("It has already been grabbed! Select any other");
+                            case 1:
+                                Console.WriteLine("It has already been grabbed! Select any other.");
                                 break;
                             case 2:
+                                draw++;
                                 next = 1;
                                 Console.Clear();
                                 game.displayGrid();
                                 break;
-                            case 3: Console.WriteLine("Hurray!! Player 1 won the game.\nPlease press any key to restart!!");
+                            case 3:
+                                Console.WriteLine("\nHurray!! Player 1 won the game.\nPlease press any key to restart!!");
                                 Console.ReadKey();
                                 Console.Clear();
                                 game.createGrid();
                                 game.displayGrid();
-                                break; 
+                                draw = 0;
+                                break;
                         }
                         if (next == 1)
                             break;
@@ -45,28 +61,42 @@ namespace TicTacToe
                 next = 0;
                 while (true)
                 {
-                    Console.Write("\nPlayer-2 ->Enter box number to grab it: ");
+                    if(draw<9)
+                        Console.Write("\nPlayer-2 ->Enter box number to grab it: ");
+                    else
+                    {
+                        Console.WriteLine("Round DRAW!!!.\nPlease press any key to restart!!");
+                        Console.ReadKey();
+                        Console.Clear();
+                        game.createGrid();
+                        game.displayGrid();
+                        draw = 0;
+                        break;
+                    }
+
                     bool valid = int.TryParse(Console.ReadLine(), out choice2);
                     if (valid && choice2 > 0 && choice2 <= 9)
                     {
-                        int check = game.playChance(2,choice2);
+                        int check = game.playChance(2, choice2);
                         switch (check)
                         {
                             case 1:
-                                Console.WriteLine("It has already been grabbed! Select any other");
+                                Console.WriteLine("It has already been grabbed! Select any other."+draw);
                                 break;
                             case 2:
+                                draw++;
                                 next = 1;
                                 Console.Clear();
                                 game.displayGrid();
                                 break;
                             case 3:
-                                Console.WriteLine("Hurray!! Player 2 won the game.\nPlease press any key to restart!!");
+                                Console.WriteLine("\nHurray!! Player 2 won the game.\nPlease press any key to restart!!");
                                 Console.ReadKey();
                                 Console.Clear();
                                 game.createGrid();
                                 game.displayGrid();
                                 next = 1;
+                                draw = 0;
                                 break;
                         }
                         if (next == 1)
