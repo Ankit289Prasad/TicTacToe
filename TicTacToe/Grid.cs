a2ca343347cf1feacc2b058ee7ac572ba489b329
Ankit Prasad <ankit28prasad9@gmail.com>
2022-03-06 19:55:26 +0530

    Add project files.

---
+++ TicTacToe/Grid.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Grid
+    {
+        private char[,] grid = new char[3, 3];
+        public void createGrid()
+        {
+            int c = 49;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    grid[i, j] = (char)c;
+                    c++;
+                }
+        }
+        public void displayGrid()
+        {
+            Console.WriteLine(" +==========================+");
+            for (int i = 0; i < 3; i++)
+            {
+                
+                Console.WriteLine(" |        :        :        |");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(" |   "+grid[i, j]+"   ");
+                }
+                Console.WriteLine(" |\n |        :        :        |");
+                if(i<2)
+                Console.WriteLine(" ----------------------------");
+            }
+            Console.WriteLine(" +==========================+");
+        }
+        public int playChance(int playerNumber,int choice)
+        {
+            int status=0;
+            switch(choice)
+            {
+                case 1:
+                    if (grid[0, 0] == 49)
+                    {
+                        if (playerNumber == 1)
+                            grid[0, 0] = 'X';
+                        else
+                            grid[0, 0] ='O';
+                        status=checkWinning();
+                    }
+                    else
+                        status = 1;
+                    break;
+                case 2:
+                    if (grid[0, 1] == 50)
+                    {
+                        if (playerNumber == 1)
+                            grid[0, 1] = 'X';
+                        else
+                            grid[0, 1] = 'O';
+                        status = checkWinning();
+                    }
+                    else
+                        status = 1;
+                    break;
+                case 3:
+                    if (grid[0, 2] == 51)
+                    {
+                        if (playerNumber == 1)
+                            grid[0, 2] = 'X';
+                        else
+                            grid[0, 2] = 'O';
+                        status = checkWinning();
+                    }
+                    else
+                        status = 1;
+                    break;
+                case 4:
+                    if (grid[1,0] == 52)
+                    {
+                        if (playerNumber == 1)
+                            grid[1, 0] = 'X';
+                        else
+                            grid[1, 0] = 'O';
+                        status = checkWinning();
+                    }
+                    else
+                        status = 1;
+                    break;
+                case 5:
+                    if (grid[1,1] == 53)
+                    {
+                        if (playerNumber == 1)
+                            grid[1,1] = 'X';
+                        else
+                            grid[1,1] = 'O';
+                        status = checkWinning();
+                    }
+                    else
+                        status = 1;
+                    break;
+                case 6:
+                    if (grid[1,2] == 54)
+                    {
+                        if (playerNumber == 1)
+                            grid[1, 2] = 'X';
+                        else
+                            grid[1, 2] = 'O';
+                        status = checkWinning();
+                    }
+                    else
+                        status = 1;
+                    break;
+                case 7:
+                    if (grid[2,0] == 55)
+                    {
+                        if (playerNumber == 1)
+                            grid[2, 0] = 'X';
+                        else
+                            grid[2, 0] = 'O';
+                        status = checkWinning();
+                    }
+                    else
+                        status = 1;
+                    break;
+                case 8:
+                    if (grid[2, 1] == 56)
+                    {
+                        if (playerNumber == 1)
+                            grid[2, 1] = 'X';
+                        else
+                            grid[2, 1] = 'O';
+                        status = checkWinning();
+                    }
+                    else
+                        status = 1;
+                    break;
+                case 9:
+                    if (grid[2, 2] == 57)
+                    {
+                        if (playerNumber == 1)
+                            grid[2, 2] = 'X';
+                        else
+                            grid[2, 2] = 'O';
+                        status = checkWinning();
+                    }
+                    else
+                        status = 1;
+                    break;
+            }
+            return status;
+        }
+        public int checkWinning()
+        {
+            int status = 2;
+            if ((grid[0, 0] == grid[1, 1] && grid[1, 1] == grid[2, 2]) || (grid[0, 0] == grid[0, 1] && grid[0, 1] == grid[0, 2]) || ((grid[0, 0] == grid[1, 0] && grid[1, 0] == grid[2, 0])) || (grid[0, 1] == grid[1, 1] && grid[2, 1] == grid[1, 1]) || (grid[0, 2] == grid[1, 2] && grid[1, 2] == grid[2, 2]) || (grid[0, 2] == grid[1, 1] && grid[2, 0] == grid[1, 1]) || (grid[1, 0] == grid[1, 1] && grid[1, 1] == grid[1, 2]) || (grid[2, 0] == grid[2, 1] && grid[2, 1] == grid[2, 2]))
+                status = 3;
+            if (status == 3)
+            {
+                Console.Clear();
+                displayGrid();
+            }
+            return status;
+        }
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grid game = new Grid();
+            game.createGrid();
+            game.displayGrid();
+            int choice1, choice2,next=0;
+            while (true)
+            {
+                next = 0;
+                while (true)
+                {
+                    Console.Write("\nPlayer-1 ->Enter box number to grab it: ");
+                    bool valid = int.TryParse(Console.ReadLine(), out choice1);
+                    if (valid && choice1>0&&choice1<=9)
+                    {
+                        int check=game.playChance(1,choice1);
+                        switch (check)
+                        {
+                            case 1: Console.WriteLine("It has already been grabbed! Select any other");
+                                break;
+                            case 2:
+                                next = 1;
+                                Console.Clear();
+                                game.displayGrid();
+                                break;
+                            case 3: Console.WriteLine("Hurray!! Player 1 won the game.\nPlease press any key to restart!!");
+                                Console.ReadKey();
+                                Console.Clear();
+                                game.createGrid();
+                                game.displayGrid();
+                                break; 
+                        }
+                        if (next == 1)
+                            break;
+                    }
+                    else
+                        Console.WriteLine("Please enter valid box number!");
+                }
+                next = 0;
+                while (true)
+                {
+                    Console.Write("\nPlayer-2 ->Enter box number to grab it: ");
+                    bool valid = int.TryParse(Console.ReadLine(), out choice2);
+                    if (valid && choice2 > 0 && choice2 <= 9)
+                    {
+                        int check = game.playChance(2,choice2);
+                        switch (check)
+                        {
+                            case 1:
+                                Console.WriteLine("It has already been grabbed! Select any other");
+                                break;
+                            case 2:
+                                next = 1;
+                                Console.Clear();
+                                game.displayGrid();
+                                break;
+                            case 3:
+                                Console.WriteLine("Hurray!! Player 2 won the game.\nPlease press any key to restart!!");
+                                Console.ReadKey();
+                                Console.Clear();
+                                game.createGrid();
+                                game.displayGrid();
+                                next = 1;
+                                break;
+                        }
+                        if (next == 1)
+                            break;
+                    }
+                    else
+                        Console.WriteLine("Please enter valid box number!");
+                }
+            }
+        }
+    }
+}
